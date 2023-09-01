6f8f6bf7260358d74ac38839366dce3cab9df949
Пользователь <01txxt10@gmail.com>
2023-09-01 10:31:41 +0700

    Размечает и стилизует блок 'Программы'

--- source/js/modules/sliders/sliders.js
+++ source/js/modules/sliders/sliders.js
@@ -13,6 +13,36 @@ function initSliders() {
       clickable: true,
     },
   });
+
+  const programsSlider = new Swiper('.programs__slider', {
+    direction: 'horizontal',
+    loop: false,
+    allowTouchMouse: false,
+    slidesPerView: 3,
+    spaceBetween: 32,
+    breakpoints: {
+      0: {
+        allowTouchMouse: true,
+        slidesPerView: 1,
+      },
+      768: {
+        allowTouchMouse: false,
+        slidesPerView: 2.13,
+        spaceBetween: 30,
+      },
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 32,
+      },
+    },
+    navigation: {
+      nextEl: '.programs__slider-next',
+      prevEl: '.programs__slider-prev',
+    },
+    scrollbar: {
+      el: '.programs__slider-scrollbar',
+    },
+  });
 }
 
 export default initSliders;
